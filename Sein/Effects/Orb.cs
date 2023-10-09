2c1465aa834a140010ab9bcff884aa3c4eb1326f
dplochcoder <daniel.ploch.coder@gmail.com>
2023-10-09 18:25:21 -0400

    Move Orb to foreground

--- Sein/Effects/Orb.cs
+++ Sein/Effects/Orb.cs
@@ -26,7 +26,7 @@ internal class Orb : MonoBehaviour
     }
 
     private static float SCALE = 0.6f;
-    private static float Z = 0.1f;
+    private static float Z = -0.01f;
     private static float ACCEL = 21f;
     private static float MAX_SPEED = 60f;
     private static float MAX_IDLE_VELOCITY = 5f;
